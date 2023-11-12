fdb18b8d67487198c530ffd8e73f47178862f975
lxsxl <140750201+lxsxl@users.noreply.github.com>
2023-11-12 19:48:30 +0100

    Revert "Update language.h"

--- src/language.h
+++ src/language.h
@@ -1468,7 +1468,7 @@ static const char* const myLanguage[18][208] = {
     "Erkannte Applikationen", // 204
     "Min.", // 205
     "Dyn. SPI-Geschwind.", // 206
-    "Scan Empfindlichkeit" // 207
+    "Scan sensitivity" // 207
   },
 
   { "Český", // Czech
