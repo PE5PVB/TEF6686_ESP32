a16457d7076b5bb2bc3bb73a0c970c96634b3f05
Sjef Verhoeven PE5PVB <pe5pvb@het-bar.net>
2024-05-05 21:51:38 +0200

    Disabled Chine model by default

--- src/config.h
+++ src/config.h
@@ -4,6 +4,6 @@
 #include <Arduino.h>
 
 // #define HAS_AIR_BAND        // uncomment to enable Air Band(Make sure you have Air Band extend board)
-#define CHINA_PORTABLE      // uncomment for China Portable build (Simplified Chinese)
+// #define CHINA_PORTABLE      // uncomment for China Portable build (Simplified Chinese)
 
 #endif
