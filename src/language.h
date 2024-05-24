48c7689eff691fbced92a2abf794924b8565b3b5
Sjef Verhoeven PE5PVB <pe5pvb@het-bar.net>
2024-05-25 00:05:34 +0200

    Versioning

--- src/language.h
+++ src/language.h
@@ -1,7 +1,7 @@
 #ifndef LANGUAGE_H
 #define LANGUAGE_H
 
-#define VERSION "v2.10 RC5"
+#define VERSION "v2.10 RC6"
 
 // [number of languages][number of texts]
 // *** means the text is the same as in English
