e0f280e2652c8e026165edfefc7002b6cebb0dd4
Russell Dwarshuis <kb8u_vhf@hotmail.com>
2025-01-08 10:36:18 -0500

    added NTPupdate

---
+++ src/NTPupdate.cpp
@@ -0,0 +1,58 @@
+#include "NTPupdate.h"
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address) {
+  byte packetBuffer[NTP_PACKET_SIZE];
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+
+time_t getNtpTime() {
+  IPAddress ntpServerIP; // NTP server's ip address
+  byte packetBuffer[NTP_PACKET_SIZE];
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL;
+    }
+  }
+  return 0; // return 0 if unable to get the time
+}
+
+void NTPupdate() {
+  if (!wifi) { return; }
+  time_t time = getNtpTime();
+  if (time) {
+    rtc.setTime(time);
+  }
+}
+
+
