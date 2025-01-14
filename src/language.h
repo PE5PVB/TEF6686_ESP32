a02e9f5ac0b428131683dc67673e4a0ce4ce15d6
Sjef Verhoeven PE5PVB <76666293+PE5PVB@users.noreply.github.com>
2025-01-14 16:17:20 +0100

    Update language.h

--- src/language.h
+++ src/language.h
@@ -2108,7 +2108,7 @@ static const char* const myLanguage[18][300] PROGMEM = {
     "Logbuch herunterladen", // 287
     "Logbuch ist leer", // 288
     "Nach unten gehen", // 289
-    "Zum Logbuch hinzugefügt", // 290
+    "Log erstellt", // 290
     "Update fehlgeschlagen", // 291
     "Logbuch löschen", // 292
     "Logbuch gelöscht", // 293
