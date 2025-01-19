2dfd23a468fd7a6e80c6b00f6c3c9736b62fb93e
Sjef Verhoeven PE5PVB <pe5pvb@het-bar.net>
2025-01-19 18:55:28 +0100

    Moved logbook to seperate cpp

---
+++ src/logbook.h
@@ -0,0 +1,35 @@
+#ifndef LOGBOOK_H
+#define LOGBOOK_H
+
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <SPIFFS.h>
+#include "TEF6686.h"
+
+extern bool autoDST;
+extern bool clockampm;
+extern bool NTPupdated;
+extern bool rtcset;
+extern byte language;
+extern byte unit;
+extern int16_t SStatus;
+extern int8_t NTPoffset;
+extern unsigned int ConverterSet;
+extern unsigned int frequency;
+extern unsigned int logcounter;
+
+extern TEF6686 radio;
+extern WebServer webserver;
+
+void handleRoot();
+void handleDownloadCSV();
+bool handleCreateNewLogbook();
+byte addRowToCSV();
+String getCurrentDateTime();
+bool isDST(time_t t);
+void handleLogo();
+void printLogbookCSV();
+
+#endif
