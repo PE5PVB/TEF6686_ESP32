92c4f043930bcc5d7a3de46ce1f0ab9e9cee3ec9
Sjef Verhoeven PE5PVB <pe5pvb@het-bar.net>
2025-03-19 11:56:26 +0100

    Optimise default settings

--- src/config.h
+++ src/config.h
@@ -4,7 +4,6 @@
 #include <Arduino.h>
 
 // #define HAS_AIR_BAND        // uncomment to enable Air Band(Make sure you have Air Band extend board)
-// #define CHINA_PORTABLE      // uncomment for China Portable build (Simplified Chinese)
 // #define DEEPELEC_DP_66X     // uncomment for DEEPELEC Portable DP-66X build (Simplified Chinese)
 
 #endif
