81defbb8cc4c6de153910592ef4a6136f8485669
Sjef Verhoeven PE5PVB <76666293+PE5PVB@users.noreply.github.com>
2026-02-20 21:18:56 +0100

    Versioning

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-variable"
 
-#define VERSION "v2.20.7"
+#define VERSION "v2.20.8"
 
 #define ON                          1
 #define OFF                         0
